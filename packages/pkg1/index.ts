107cc83d059deb4c7df39d545f8a99a538a7810b
Yuheng Ouyang <govizlora@gmail.com>
2020-05-21 10:26:17 -0700

    Use monorepo

---
+++ packages/pkg1/index.ts
@@ -0,0 +1,5 @@
+import type { NumType } from '../project_b/utils/utils'
+
+const a: NumType = 42;
+
+console.log(a);
--- project_a/index.ts
+++
@@ -1,5 +0,0 @@
-import type { NumType } from '../project_b/build/utils/utils'
-
-const a: NumType = 42;
-
-console.log(a);
